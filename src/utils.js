3cbbd254e5d3ae75f017e1b2f2304dcc78e888eb
Kenanalhennawi <135068266+Kenanalhennawi@users.noreply.github.com>
2026-01-25 02:33:41 +0400

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,23 +1,24 @@
+export const n = (s) => (s || "").toString().toLowerCase();
+
 export const esc = (s) =>
-  String(s ?? "")
-    .replace(/&/g, "&amp;")
-    .replace(/</g, "&lt;")
-    .replace(/>/g, "&gt;")
-    .replace(/"/g, "&quot;")
-    .replace(/'/g, "&#39;");
+  (s || "")
+    .toString()
+    .replaceAll("&", "&amp;")
+    .replaceAll("<", "&lt;")
+    .replaceAll(">", "&gt;")
+    .replaceAll('"', "&quot;")
+    .replaceAll("'", "&#039;");
 
-export const uniq = (arr) => Array.from(new Set((arr || []).filter(Boolean)));
+export const escAttr = (s) =>
+  (s || "")
+    .toString()
+    .replaceAll("&", "&amp;")
+    .replaceAll("<", "&lt;")
+    .replaceAll(">", "&gt;")
+    .replaceAll('"', "&quot;")
+    .replaceAll("'", "&#039;");
 
-export const wrapToken = (t) => `<span class="token">${esc(t)}</span>`;
+export const uniq = (arr) => [...new Set(arr || [])];
 
-export const normalize = (s) =>
-  String(s ?? "")
-    .replace(/\u0000/g, "")
-    .replace(/\r\n?/g, "\n")
-    .trim();
-
-export const tokenize = (text) =>
-  normalize(text)
-    .split(/[\s]+/)
-    .map((x) => x.trim())
-    .filter(Boolean);
+export const wrapToken = (token) =>
+  `<code data-token="${escAttr(token)}">${esc(token)}</code>`;
